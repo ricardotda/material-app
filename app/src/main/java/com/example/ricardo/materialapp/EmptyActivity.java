b23a95fdde54ae74a5b98cda3f9d8f575da8ffcb
Ricardo T. de Azevedo <ricardo.azevedo@solvian.com>
2016-09-13 19:30:15 -0300

    commit

---
+++ app/src/main/java/com/example/ricardo/materialapp/EmptyActivity.java
@@ -0,0 +1,13 @@
+package com.example.ricardo.materialapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+    }
+}
